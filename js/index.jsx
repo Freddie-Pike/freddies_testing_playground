9cefd65d77009e9b0c447991ff774a1054ef95b2
Freddie Pike <Freddie@colabsoftware.com>
2020-11-16 18:05:38 -0330

    Remove broken Modal code.

--- js/index.jsx
+++ js/index.jsx
@@ -1,25 +1,19 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
-import Modal from "./components/Modal/Modal";
 import Button from "./components/Button/Button";
 import GreeterUI from "./components/GreeterUI/GreeterUI";
 
 function App() {
-  const [showModal, setShowModal] = useState(false);
-
   return (
     <div className="main-page-wrapper">
       <Button
         onClick={() => {
-          setShowModal(true);
+          alert("Test!");
         }}
       >
-        Show a cool modal!
+        Test Button
       </Button>
       <GreeterUI person="Fred" />
-      <Modal show={showModal} onClickOutside={() => setShowModal(false)}>
-        Close the modal by clicking outside of the modal : )
-      </Modal>
     </div>
   );
 }
